63a0cde85dadfec005be8ab61b1706ad6bf2b457
Arkasha Ivanov (apkawa) <apkawa@gmail.com>
2021-07-28 10:46:13 +0300

    Try fix codecov

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,12 @@
-# pytest_plugins = ['pytest_image_diff.plugin']
+import os
+
+import pytest
+from _pytest.fixtures import FixtureRequest
+
+
+@pytest.fixture(scope="session")
+def image_diff_root(request: FixtureRequest) -> str:
+    """
+    Root path for storing diff images. By default - `request.config.rootdir`
+    """
+    return os.path.join(request.config.rootdir, "tests")
