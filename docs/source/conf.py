8ab52fbd0d2f588be23c3e3f929ec4c14cdd065c
apkawa <apkawa@gmail.com>
2023-01-29 18:22:01 +0300

    chore: Bump version: 0.0.9 → 0.0.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = "Apkawa <apkawa@gmail.com"
 # The short X.Y version
 version = "0.0"
 # The full version, including alpha/beta/rc tags
-release = "0.0.9"
+release = "0.0.10"
 
 # -- General configuration ---------------------------------------------------
 
